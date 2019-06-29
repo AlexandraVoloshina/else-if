50e2fb9d8034faee89754e5cd29667217b11c8d2
Alexandra <alexandrita@ukr.net>
2019-06-29 13:25:58 +0300

    Update script,js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var age = prompt("Сколько лет?");
 var smoke = confirm("Куришь?");
++smoke;
 
-if (age<18 && smoke==false) 
+if (age<18 && smoke!=1) 
 	{
 	alert("Так держать!");
-	} else if(age<18 && smoke==true) {
+	} else if(age<18 && smoke!=0) {
 		alert("Маме скажу!");
-			} else if(age>18 && smoke==false) {
+			} else if(age>18 && smoke!=1) {
 				alert("Молодец, и не надо!");
 			} 	else 
 					alert("Ну и зря!");
